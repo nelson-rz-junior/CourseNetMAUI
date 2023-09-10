2af2482773152e26c2459272d4436f880f628799
Nelson Roza Junior <jr.1403@hotmail.com>
2023-09-09 23:20:55 -0300

    AppContentPage / AppNavigationPage

---
+++ AppNavigationPage/Page2.xaml.cs
@@ -0,0 +1,19 @@
+namespace AppNavigationPage;
+
+public partial class Page2 : ContentPage
+{
+	public Page2()
+	{
+		InitializeComponent();
+	}
+
+    private void ButtonNext_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new Page3());
+    }
+
+    private void ButtonBack_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+}
