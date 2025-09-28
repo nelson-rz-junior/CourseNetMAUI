78786014131fe77994de1530c0d308e0604e291d
Nelson Roza Junior <nelson.jr.1403@hotmail.com>
2025-09-28 15:05:15 -0300

    Elementos de lista: ListView

--- AppMauiGallery/Views/Lists/ListViewPage.xaml.cs
+++ AppMauiGallery/Views/Lists/ListViewPage.xaml.cs
@@ -11,8 +11,7 @@ public partial class ListViewPage : ContentPage
 
     private void btnCarregarFilmes_Clicked(object sender, EventArgs e)
     {
-		ctrlListView.ItemsSource = MovieList.GetList();
-        btnCarregarFilmes.IsEnabled = false;
+		ctrlListView.ItemsSource = MovieList.GetGroupList();
     }
 
     private void ctrlListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -23,5 +22,30 @@ public partial class ListViewPage : ContentPage
             DisplayAlert("Filme Selecionado", $"Você selecionou o filme: {movie.Name}", "OK");
             ctrlListView.SelectedItem = null;
         }
+    }
+
+    private async void ctrlListView_Refreshing(object sender, EventArgs e)
+    {
+        ctrlListView.IsRefreshing = true;
+
+        await Task.Delay(2000);
+
+        List<GroupMovie> movies = MovieList.GetGroupList();
+        var disneyGroup = movies.FirstOrDefault(x => x.CompanyName == "Disney");
+
+        Movie newMovie = new()
+        {
+            Id = 99,
+            Name = "Novo Filme acionado pelo refresh",
+            Description = "Este filme somente é adicionado à lista inicial quando o refresh do ListView é acionado manualmente. Para recarregar a lista inicial, basta clicar no botão 'Carregar Filmes'.",
+            LaunchYear = 2025,
+            Duration = new TimeSpan(0, 10, 0)
+        };
+
+        disneyGroup.Insert(0, newMovie);
+
+        ctrlListView.ItemsSource = movies;
+
+        ctrlListView.IsRefreshing = false;
     }
 }
